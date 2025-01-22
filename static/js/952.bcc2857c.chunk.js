fef819506b546d9374b4e47e0084ff53ab744abb
Chmelyuk <98876964+Chmelyuk@users.noreply.github.com>
2025-01-22 22:49:13 +0200

    Updates

---
+++ static/js/952.bcc2857c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_new_react_app=self.webpackChunkmy_new_react_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.bcc2857c.chunk.js.map
